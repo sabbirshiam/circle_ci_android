0a333e98aac0dbebe0350df22e4d115f7bd13204
sabbirshiam <sabbirshiam@gmail.com>
2020-05-31 22:14:17 +0900

    testing 2

--- app/src/main/java/com/ssh/circlecisetup/ThirdFragment.kt
+++ app/src/main/java/com/ssh/circlecisetup/ThirdFragment.kt
@@ -1,13 +1,10 @@
 package com.ssh.circlecisetup
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import java.util.concurrent.TimeUnit
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SecondFragment.newInstance] factory method to
+ * Use the [ThirdFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SecondFragment : Fragment() {
+class ThirdFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -32,29 +29,12 @@ class SecondFragment : Fragment() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Handler().postDelayed(
-            {
-                fragmentManager?.let {
-                    val fragmentTransaction = it
-                        .beginTransaction()
-                        .setCustomAnimations(R.anim.slide_in, R.anim.slide_out)
-                        .replace(R.id.fragment_container_view, ThirdFragment.newInstance("", ""))
-
-                    // .addToBackStack(null)
-                    fragmentTransaction.commit()
-                }
-            }, 1000L
-        )
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_third, container, false)
     }
 
     companion object {
@@ -64,12 +44,12 @@ class SecondFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment SecondFragment.
+         * @return A new instance of fragment ThirdFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SecondFragment().apply {
+            ThirdFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
