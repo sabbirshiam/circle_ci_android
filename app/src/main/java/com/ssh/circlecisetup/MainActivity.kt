4d58d6b2bb86d86a7f58ff9aaa0afb68a2049c6e
sabbirshiam <sabbirshiam@gmail.com>
2020-05-31 18:10:34 +0900

    testing with fragment and android activity style transparent.

--- app/src/main/java/com/ssh/circlecisetup/MainActivity.kt
+++ app/src/main/java/com/ssh/circlecisetup/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.ssh.circlecisetup
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            val w: Window = window
+//            w.setFlags(
+//                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+//                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+//            )
+//        }
         setContentView(R.layout.activity_main)
 
+
+        val fragmentTransaction = supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(R.anim.slide_in, R.anim.slide_out)
+            .replace(R.id.fragment_container_view, FirstFragment.newInstance("", ""))
+        // .addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
